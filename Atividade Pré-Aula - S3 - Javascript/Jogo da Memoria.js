1bac14753a6715e24430144b5dafcec79c44d8b1
Aluno Lab. 09 <aluno09@aeelabs.edu.br>
2023-03-14 21:33:29 -0300

    modified:   "Atividade Pr\303\251-Aula - S3 - Javascript/Jogo da Memoria.js"

--- Atividade Pré-Aula - S3 - Javascript/Jogo da Memoria.js
+++ Atividade Pré-Aula - S3 - Javascript/Jogo da Memoria.js
@@ -3,3 +3,31 @@
 //Dev: Douglas Leonel de Almeida
 //DATA: 14/03/2023
 
+
+function selecionar(card);
+    alert(card);
+    +.96395
+
+    67/*89849
+    8
+    9889789
+    789
+
+    8
+    89
+
+
+    7789
+52
+5
+3
+
+33*/
+561
+
+1231233
+5
+232
+0,5
+5
+5201
