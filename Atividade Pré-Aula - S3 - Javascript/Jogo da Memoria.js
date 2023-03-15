d53c0ec71feabe1b9e18a0937701faf61cab296f
YondaimeDinho <DouglasLuis2017@outlook.com.br>
2023-03-15 01:10:43 -0300

    modified:   "Atividade Pr\303\251-Aula - S3 - Javascript/Jogo da Memoria.js" 	new file:   "Atividade Pr\303\251-Aula - S3 - Javascript/img1.jpg" 	new file:   "Atividade Pr\303\251-Aula - S3 - Javascript/img2.jpg" 	new file:   "Atividade Pr\303\251-Aula - S3 - Javascript/img3.jpg" 	new file:   "Atividade Pr\303\251-Aula - S3 - Javascript/img4.jpg" 	new file:   "Atividade Pr\303\251-Aula - S3 - Javascript/img5.jpg" 	new file:   "Atividade Pr\303\251-Aula - S3 - Javascript/img6.jpg" 	new file:   "Atividade Pr\303\251-Aula - S3 - Javascript/img7.jpg" 	new file:   "Atividade Pr\303\251-Aula - S3 - Javascript/img8.jpg" 	modified:   "Atividade Pr\303\251-Aula - S3 - Javascript/index.html" 	modified:   "Atividade Pr\303\251-Aula - S3 - Javascript/style.css"

--- Atividade Pré-Aula - S3 - Javascript/Jogo da Memoria.js
+++ Atividade Pré-Aula - S3 - Javascript/Jogo da Memoria.js
@@ -3,86 +3,120 @@
 //Dev: Douglas Leonel de Almeida
 //DATA: 14/03/2023
 
-// definir as variáveis
-var cartasViradas = [];
-var cartasCorretas = 0;
-var tempo = 60;
-var cartas = [{
-        id: 1,
-        img: 'caminho-da-imagem-1'
-    },
-    {
-        id: 2,
-        img: 'caminho-da-imagem-2'
-    },
-];
-// embaralhar as cartas
-cartas = cartas.concat(cartas);
-cartas.sort(function() {
-    return 0.5 - Math.random();
-});
-// criar as cartas no HTML
-var tabuleiro = document.getElementById('tabuleiro');
-for(var i = 0; i < cartas.length; i++) {
-    var carta = document.createElement('div');
-    carta.dataset.id = cartas[i].id;
-    carta.dataset.img = cartas[i].img;
-    carta.addEventListener('click', virarCarta);
-    tabuleiro.appendChild(carta);
-}
-// iniciar o contador de tempo
-var contadorTempo = setInterval(function() {
-    tempo--;
-    document.getElementById('tempo').innerHTML = tempo;
-    if(tempo === 0) {
-        clearInterval(contadorTempo);
-        gameOver();
+// Crie uma lista com as imagens dos pares de cartas
+function startGame() {
+
+    const cardImages = [
+        "img1.jpg",
+        "img2.jpg",
+        "img3.jpg",
+        "img4.jpg",
+        "img5.jpg",
+        "img6.jpg",
+        "img7.jpg",
+        "img8.jpg"
+    ];
+
+    // Defina o número de cartas no jogo
+    const numCards = cardImages.length * 2;
+
+    // Crie uma lista com os números das cartas
+    const cardNumbers = [];
+    for (let i = 0; i < numCards; i++) {
+        cardNumbers.push(i % cardImages.length);
     }
-}, 1000);
-// função para virar a carta
-function virarCarta() {
-    if(cartasViradas.length < 2 && !this.classList.contains('virada')) {
-        cartasViradas.push(this);
-        this.style.backgroundImage = 'url(' + this.dataset.img + ')';
-        this.classList.add('virada');
-        if(cartasViradas.length === 2) {
-            setTimeout(verificarCartas, 1000);
+
+    // Embaralhe a lista de números de cartas
+    for (let i = cardNumbers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cardNumbers[i], cardNumbers[j]] = [cardNumbers[j], cardNumbers[i]];
+    }
+
+    // Crie a estrutura HTML das cartas
+    const gameContainer = document.querySelector(".game-container");
+    for (let i = 0; i < numCards; i++) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.dataset.cardIndex = i;
+        card.innerHTML =
+            `<div class="front"></div>
+    <div class="back" style="background-image: url('img/${cardImages[cardNumbers[i]]}');"></div>`;
+        gameContainer.appendChild(card);
+    }
+
+    // Defina as variáveis do jogo
+    let numMatches = 0;
+    let numClicks = 0;
+    let firstCard = null;
+    let secondCard = null;
+    let gameTimer = null;
+    let remainingTime = 60;
+
+    // Defina a função para virar uma carta
+    function flipCard(card) {
+        if (card.classList.contains("flipped")) {
+            return;
+        }
+        card.classList.add("flipped");
+        if (!firstCard) {
+            firstCard = card;
+            return;
+        }
+        secondCard = card;
+        numClicks++;
+        if (firstCard.dataset.cardIndex === secondCard.dataset.cardIndex) {
+            // Encontrou um par de cartas
+            firstCard = null;
+            secondCard = null;
+            numMatches++;
+            if (numMatches === cardImages.length) {
+                // Ganhou o jogo
+                endGame(true);
+            }
+        } else {
+            // Não encontrou um par de cartas
+            disableCards();
+            setTimeout(() => {
+                firstCard.classList.remove("flipped");
+                secondCard.classList.remove("flipped");
+                enableCards();
+                firstCard = null;
+                secondCard = null;
+                if (confirm("Você deseja jogar novamente?")) {
+                    // Perdeu o jogo
+                    endGame(false);
+                }
+            }, 1000);
         }
     }
-}
-// função para verificar se as cartas são iguais
-function verificarCartas() {
-    var carta1 = cartasViradas[0];
-    var carta2 = cartasViradas[1];
-    if(carta1.dataset.id === carta2.dataset.id) {
-        carta1.style.backgroundColor = '#00ff00';
-        carta2.style.backgroundColor = '#00ff00';
-        carta1.removeEventListener('click', virarCarta);
-        carta2.removeEventListener('click', virarCarta);
-        cartasCorretas++;
-        if(cartasCorretas === cartas.length / 2) {
-            clearInterval(contadorTempo);
-            gameOver();
-        }
+
+    // Defina as funções para habilitar/desabilitar todas as cartas
+    function disableCards() {
+        const cards = document.querySelectorAll(".card");
+        cards.forEach(card => card.style.pointerEvents = "none");
     }
-    else {
-        carta1.style.backgroundImage = '';
-        carta2.style.backgroundImage = '';
-        carta1.classList.remove('virada');
-        carta2.classList.remove('virada');
+
+    function enableCards() {
+        const cards = document.querySelectorAll(".card:not(.flipped)");
+        cards.forEach(card => card.style.pointerEvents = "auto");
     }
-    cartasViradas = [];
+
+    // Defina a função para atualizar o tempo restante
+    function updateTimer() {
+        remainingTime--;
+        const timerElement = document.getElementById("timer");
+        timerElement.textContent = remainingTime + " segundos";
+        if (remainingTime === 0) {
+            // Perdeu o jogo
+            endGame(false);
+        };
+
+        // Adicione um ouvinte de eventos para o botão "Jogar novamente"
+        const playAgainButton = document.querySelector(".play-again");
+        playAgainButton.addEventListener("click", startGame);
+    }
+
+    // Chame a função startGame() quando a página for carregada
+    window.addEventListener("load", startGame);
+
 }
-// função para mostrar a tela de resultados
-function gameOver() {
-    var mensagem = document.createElement('div');
-    mensagem.innerHTML = 'Você ' + (cartasCorretas === cartas.length / 2 ? 'ganhou' : 'perdeu') + ' o jogo!';
-    var botaoReiniciar = document.createElement('button');
-    botaoReiniciar.innerHTML = 'Reiniciar';
-    botaoReiniciar.addEventListener('click', reiniciarJogo);
-    var telaResultado = document.createElement('div');
-    telaResultado.appendChild(mensagem);
-    telaResultado.appendChild(botaoReiniciar);
-    document.body.appendChild(telaResultado);
-}
-// função para reiniciar o jogo
